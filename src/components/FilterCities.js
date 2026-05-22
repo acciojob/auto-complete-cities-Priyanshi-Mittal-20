449add45cfb647191cdc80743204062ed4e9c6aa
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-05-22 23:09:47 +0530

    auto-2

--- src/components/FilterCities.js
+++ src/components/FilterCities.js
@@ -1,27 +1,52 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const FilterCities = ({suggestions}) => {
+const FilterCities = ({ suggestions }) => {
 
-const [filter,setFilter]=useState([])
-function handleChange(e)
-{
-  setFilter(suggestions.filter(cty=> cty.toLowerCase().includes(e.target.value.toLowerCase())))
-}
+  const [filter, setFilter] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChange(e) {
+    const value = e.target.value;
+
+    setInputValue(value);
+
+    setFilter(
+      suggestions.filter(cty =>
+        cty.toLowerCase().includes(value.toLowerCase())
+      )
+    );
+  }
+
+  function handleClick(city) {
+    setInputValue(city);
+    setFilter([]);
+  }
+
   return (
     <div>
-      <label for="city">Search cities of India:</label>
-      <input id="city" type="text" onChange={handleChange} />
-      {
-        filter &&
-        <div>
-            {filter.map(cty=>
-              <li>{cty}</li>
-            )}
-        </div>
-      }
+      <label htmlFor="city">Search cities of India:</label>
+
+      <input
+        id="city"
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+      />
+
+      <div>
+        {
+          filter.map((cty, index) => (
+            <li
+              key={index}
+              onClick={() => handleClick(cty)}
+            >
+              {cty}
+            </li>
+          ))
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default FilterCities
+export default FilterCities;
